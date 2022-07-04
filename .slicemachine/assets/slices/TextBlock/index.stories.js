38f625ea9159d80eaee566d3c99a0cfaaca92a01
abhi-springer <abhishek.kale.consultant@springernature.com>
2022-07-04 18:38:07 +0530

    second commit

---
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlock';
+
+export default {
+  title: 'slices/TextBlock'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Deploy transparent e-markets","spans":[]}],"description":[{"type":"paragraph","text":"Esse non id voluptate ullamco est exercitation ea. Velit quis ad laboris dolore cillum nisi in dolore nulla est in ut sint fugiat incididunt.","spans":[]}],"myText":[{"type":"paragraph","text":"Deserunt ad elit occaecat enim elit fugiat dolor nulla sit non proident nulla proident mollit. Consectetur ex ea nulla do excepteur.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
